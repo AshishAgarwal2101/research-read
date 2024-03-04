d001a23c7fc2f92411cb94a0f95956e50b5cbf9c
Ashish <ashish21.01agarwal@gmail.com>
2024-03-03 17:46:12 -0800

    Code structure

--- src/convex/_generated/api.d.ts
+++ src/convex/_generated/api.d.ts
@@ -14,7 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as tasks from "../tasks.js";
+import type * as dao_tasks from "../dao/tasks.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -25,7 +25,7 @@ import type * as tasks from "../tasks.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  tasks: typeof tasks;
+  "dao/tasks": typeof dao_tasks;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
